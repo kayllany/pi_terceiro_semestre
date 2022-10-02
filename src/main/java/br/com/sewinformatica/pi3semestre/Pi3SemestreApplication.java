fad0605df4888f667bebc52f44a78f2d2b522039
ChesterKing66 <kevin13348@gmail.com>
2022-10-01 21:25:19 -0300

    Arquivos iniciais
    - java (JDK) 19
    - spring framework
    - usar MySQL

---
+++ src/main/java/br/com/sewinformatica/pi3semestre/Pi3SemestreApplication.java
@@ -0,0 +1,13 @@
+package br.com.sewinformatica.pi3semestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pi3SemestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pi3SemestreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sewinformatica/pi3semestre/Pi3SemestreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sewinformatica.pi3semestre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi3SemestreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
