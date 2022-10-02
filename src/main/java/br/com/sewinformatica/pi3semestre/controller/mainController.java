48c852d56d1bb2167292b7f6082e0854e8e50767
ChesterKing66 <kevin13348@gmail.com>
2022-10-01 22:19:25 -0300

    modelAndView

--- src/main/java/br/com/sewinformatica/pi3semestre/controller/mainController.java
+++ src/main/java/br/com/sewinformatica/pi3semestre/controller/mainController.java
@@ -2,12 +2,15 @@ package br.com.sewinformatica.pi3semestre.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class mainController {
 
     @GetMapping("/")
-    public String main() {
-        return "main";
+    public ModelAndView main() {
+        ModelAndView mv = new ModelAndView("main");
+
+        return mv;
     }
 }
