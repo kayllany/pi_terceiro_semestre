a7ea36db6bad63fededcf1d3609778c797a7b321
ChesterKing66 <kevin13348@gmail.com>
2022-11-14 23:18:50 -0300

    listing types

--- src/main/java/br/com/sewinformatica/pi3semestre/controllers/TipoController.java
+++ src/main/java/br/com/sewinformatica/pi3semestre/controllers/TipoController.java
@@ -1,15 +1,26 @@
 package br.com.sewinformatica.pi3semestre.controllers;
 
+import br.com.sewinformatica.pi3semestre.models.Tipo;
+import br.com.sewinformatica.pi3semestre.repositories.TipoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class TipoController {
 
+    @Autowired
+    private TipoRepository tipoRepository;
+
     @GetMapping ("/tipos")
     public ModelAndView tipos() {
+        List<Tipo> tipos = this.tipoRepository.findAll();
+
         ModelAndView mv = new ModelAndView("tipos");
+        mv.addObject("tipos", tipos);
 
         return mv;
     }
