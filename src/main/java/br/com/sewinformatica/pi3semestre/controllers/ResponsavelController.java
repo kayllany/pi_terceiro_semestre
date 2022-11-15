beaf31f09aeff553d7e2543cc36bdb97edc51dd8
ChesterKing66 <kevin13348@gmail.com>
2022-11-14 22:52:03 -0300

    created route and view to list responsibles

--- src/main/java/br/com/sewinformatica/pi3semestre/controllers/ResponsavelController.java
+++ src/main/java/br/com/sewinformatica/pi3semestre/controllers/ResponsavelController.java
@@ -1,7 +1,28 @@
 package br.com.sewinformatica.pi3semestre.controllers;
 
+import br.com.sewinformatica.pi3semestre.models.Responsavel;
+import br.com.sewinformatica.pi3semestre.repositories.ResponsavelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class ResponsavelController {
+
+    @Autowired
+    private ResponsavelRepository responsavelRepository;
+
+    @GetMapping("/responsaveis")
+    public ModelAndView responsaveis() {
+        List<Responsavel> responsaveis = this.responsavelRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("responsaveis");
+        mv.addObject("responsaveis", responsaveis);
+
+        return mv;
+    }
 }
