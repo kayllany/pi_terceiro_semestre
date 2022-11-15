10fe265e602b54f54cbc934cebf5f78d9044d07c
ChesterKing66 <kevin13348@gmail.com>
2022-11-15 09:32:07 -0300

    deleting types

--- src/main/java/br/com/sewinformatica/pi3semestre/controllers/TipoController.java
+++ src/main/java/br/com/sewinformatica/pi3semestre/controllers/TipoController.java
@@ -5,6 +5,7 @@ import br.com.sewinformatica.pi3semestre.repositories.TipoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -23,5 +24,12 @@ public class TipoController {
         mv.addObject("tipos", tipos);
 
         return mv;
+    }
+
+    @GetMapping("/tipos/{id}/delete")
+    public String delete(@PathVariable Integer id) {
+        this.tipoRepository.deleteById(id);
+
+        return "redirect:/tipos";
     }
 }
