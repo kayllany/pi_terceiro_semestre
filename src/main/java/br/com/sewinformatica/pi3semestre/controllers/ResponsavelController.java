defd3817b98e3ecef0cb1191607a74613a123a67
ChesterKing66 <kevin13348@gmail.com>
2022-11-21 18:46:42 -0300

    creating responsibles

--- src/main/java/br/com/sewinformatica/pi3semestre/controllers/ResponsavelController.java
+++ src/main/java/br/com/sewinformatica/pi3semestre/controllers/ResponsavelController.java
@@ -1,11 +1,14 @@
 package br.com.sewinformatica.pi3semestre.controllers;
 
+import br.com.sewinformatica.pi3semestre.DTO.ResponsavelDTO;
+import br.com.sewinformatica.pi3semestre.enums.SetoresEnum;
 import br.com.sewinformatica.pi3semestre.models.Responsavel;
 import br.com.sewinformatica.pi3semestre.repositories.ResponsavelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -24,6 +27,22 @@ public class ResponsavelController {
         mv.addObject("responsaveis", responsaveis);
 
         return mv;
+    }
+
+    @GetMapping("/responsaveis/new")
+    public ModelAndView newResponsavel(ResponsavelDTO responsavelDTO) {
+        ModelAndView mv = new ModelAndView("responsavel/novoResponsavel");
+        mv.addObject("setores", SetoresEnum.values());
+
+        return mv;
+    }
+
+    @PostMapping("/responsaveis/create")
+    public String create(ResponsavelDTO responsavelDTO) {
+        Responsavel responsavel = responsavelDTO.toReponsavel();
+        this.responsavelRepository.save(responsavel);
+
+        return "redirect:/responsaveis";
     }
 
     @GetMapping("/responsaveis/{id}/delete")
